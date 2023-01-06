524560e9bfe7c54e1c92244bb3ca6a0e432c2c8a
Surbhi275 <72935413+Surbhi275@users.noreply.github.com>
2023-01-06 11:27:26 +0530

    Add files via upload

---
+++ my_tuple=(12,'juice','green',12,'green',.py
@@ -0,0 +1,8 @@
+my_tuple=(12,'juice','green',12,'green',"juice")#taking multiple values of samething
+print(my_tuple)
+print(type(my_tuple))
+new={'jammu':'kashmir','ladhakh':'up','punjab':'rockgarden'}
+print(new.keys())
+print(new.values())
+a=list(new.keys())
+print(a)
